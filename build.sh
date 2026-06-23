ec2c3e9ed01e4eb52ae7446c5fc0061c6e0dcd07
KimmeriA06 <cemildemir47@gmail.com>
2026-06-23 12:58:43 +0300

    Modify build script for package installations
    Updated package installation commands and requirements file.

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-apt-get update
-apt-get install -y ffmpeg
-pip install -r requirements.txt
+pip install --upgrade pip
+apt-get update && apt-get install -y ffmpeg libjpeg-dev zlib1g-dev
+pip install --no-cache-dir -r gereksinimler.txt
